5f91993842a313434c760979cdf5473601eb453a
Nitin Kumar <nitinkumar@Nitins-MacBook-Air.local>
2025-06-16 12:17:41 +0530

    Set different Calculation

---
+++ Dynamic Programming/setDiffCalc.py
@@ -0,0 +1,9 @@
+# find the number of viewers who liked Channel 1 only
+# N viewers like Channel 1
+# M viewers like Channel 2
+# L viewers like both
+
+def viewers(N, M, L):
+    return N-L
+
+print(viewers(100, 75, 50))
